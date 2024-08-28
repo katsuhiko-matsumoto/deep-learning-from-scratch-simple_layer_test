b5a233cd7162f3943455dfc5aac0afda7640ffbe
Katsuhiko Matsumoto <le-bleu@horae.dti.ne.jp>
2024-08-28 11:06:04 +0900

    modify

--- common/optimizer.py
+++ common/optimizer.py
@@ -12,6 +12,16 @@ class SGD:
         for key in params.keys():
             params[key] -= self.lr * grads[key]
 
+class SGD2:
+    '''
+    確率的勾配降下法（Stochastic Gradient Descent）
+    '''
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for i in range(len(params)):
+            params[i] -= self.lr * grads[i]
 
 class Momentum:
 
